cfddd9f155570dc2b0321009c3bf4ff208078ee2
wenodh <vinodkumara3@gmail.com>
2021-11-22 23:49:09 +0530

    bootstrap added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,15 @@
 import './App.css';
+import Footer from './components/Footer/Footer';
+import Header from './components/Header/Header';
 
 function App() {
-    return <div className="App">Notes</div>;
+    return (
+        <>
+            <Header />
+            <main style={{ minHeight: '93vh' }}></main>
+            <Footer />
+        </>
+    );
 }
 
 export default App;
