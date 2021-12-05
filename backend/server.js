689e757455b574d818ab6042b3a8dfd397951c21
wenodh <vinodkumara3@gmail.com>
2021-12-06 04:06:43 +0530

    notes added

--- backend/server.js
+++ backend/server.js
@@ -10,24 +10,24 @@ connectDb();
 
 app.use(express.json());
 
-_dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+// _dirname = path.resolve();
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
-} else {
-    app.get('/', (req, res) => {
-        res.send('Express server Running');
-    });
-}
+//     app.get('*', (req, res) =>
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//     );
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('Express server Running');
+//     });
+// }
 app.use('/api/notes', noteRoutes);
 app.use('/api/users', userRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
 });
