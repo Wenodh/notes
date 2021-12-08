86aa6860f0d33de0fb16d5a0b37da1c65ba63cb2
wenodh <vinodkumara3@gmail.com>
2021-12-09 00:59:05 +0530

    seperated backend and front end

--- backend/server.js
+++ backend/server.js
@@ -10,18 +10,18 @@ connectDb();
 
 app.use(express.json());
 
-_dirname = path.resolve();
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/build')));
+// _dirname = path.resolve();
+// if (process.env.NODE_ENV === 'production') {
+//     app.use(express.static(path.join(__dirname, '/frontend/build')));
 
-    app.get('*', (req, res) =>
-        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
-    );
-} else {
-    app.get('/', (req, res) => {
-        res.send('Express server Running');
-    });
-}
+//     app.get('*', (req, res) =>
+//         res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+//     );
+// } else {
+//     app.get('/', (req, res) => {
+//         res.send('Express server Running');
+//     });
+// }
 app.use('/api/notes', noteRoutes);
 app.use('/api/users', userRoutes);
 app.use(notFound);
@@ -30,4 +30,7 @@ app.use(errorHandler);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
+});
+app.get('/', (req, res) => {
+    res.send('Express server Running');
 });
