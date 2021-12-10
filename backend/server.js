2e7f5e4409e71caf25454573db523516b247fdac
wenodh <vinodkumara3@gmail.com>
2021-12-11 00:03:49 +0530

    added cors

--- backend/server.js
+++ backend/server.js
@@ -7,7 +7,8 @@ const path = require('path');
 require('dotenv').config();
 const app = express();
 connectDb();
-
+const cors = require('cors');
+app.use(cors());
 app.use(express.json());
 
 // _dirname = path.resolve();
@@ -22,6 +23,10 @@ app.use(express.json());
 //         res.send('Express server Running');
 //     });
 // }
+app.get('/', (req, res) => {
+    res.send('Express server Running');
+});
+
 app.use('/api/notes', noteRoutes);
 app.use('/api/users', userRoutes);
 app.use(notFound);
@@ -30,7 +35,4 @@ app.use(errorHandler);
 const port = process.env.PORT || 5000;
 app.listen(port, () => {
     console.log(`Server is running on port: ${port}`);
-});
-app.get('/', (req, res) => {
-    res.send('Express server Running');
 });
